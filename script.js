d405919fd117f8a2a79e307d0668fe434ff68eaa
Imran <imranmansuri038@gmail.com>
2022-10-17 22:47:49 +0530

    Go To Top Button Added

--- script.js
+++ script.js
@@ -23,12 +23,15 @@ addEventOnElem(navTogglers, "click", toggleNavbar);
 
 // active header  when window scroll down to 100px
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const activeElemOnScroll = function () {
   if (window.scrollY > 100) {
     header.classList.add("active");
+    backTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
+    backTopBtn.classList.remove("active");
   }
 };
 
